caaf8bcff4b8f65bbc34fba4fb01c1bd606ce1e5
20146599 <20146599@tafe.wa.edu.au>
2025-06-21 13:04:04 +0800

    feat: adding entery and exit methods for the sensor

--- src/sensor.py
+++ src/sensor.py
@@ -1,4 +1,10 @@
-class Sensor:
+from abc import ABC, abstractmethod
+
+from car_park import CarPark
+
+import random
+
+class Sensor(ABC):
     def __init__(self,
                  id: str,
                  car_park: CarPark,
@@ -7,8 +13,26 @@ class Sensor:
         self.is_active = is_active
         self.car_parl = car_park
 
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+    def _scan_plate(self):
+        return f"FAKE-{random.randint(0, 999) : 03d}"
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
 class EntrySensor:
-    ...
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor:
-    ...
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
